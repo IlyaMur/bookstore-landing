4c7e695e9491300642f0542d8933d923c59704ff
Ilya Mur <ilyamur@hotmail.com>
2021-08-06 02:39:32 +0300

    add video section

--- js/main.js
+++ js/main.js
@@ -54,4 +54,124 @@ $(document).ready(function () {
       1200: { slidesPerView: 5 }
     },
   });
+
+  //мобильное меню
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar__menu").toggleClass("navbar__menu--visible");
+    $("body").toggleClass("body__no-scroll");
+  });
+
+  //модальное окно
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key == "Escape") {
+      closeModal(event);
+    }
+  });
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: "Слишком короткое имя",
+        },
+        email: {
+          required: "Напишите ваш email",
+          email: "Формат должен быть name@domain.com.",
+        },
+        phone: {
+          required: "Пожалуйста, введите номер телефона",
+          minlength: "Формат должен быть +7 (000) 000-00-00",
+        },
+      },
+    });
+  });
+  $(document).ready(function () {
+    $(".phone").mask("+7 (000) 000-00-00");
+  });
+
+
+  $(".form__modal").each(function () {
+    $(this).validate({
+      errorClass: "error",
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: "Слишком короткое имя",
+        },
+        email: {
+          required: "Напишите ваш email",
+          email: "Формат должен быть name@domain.com.",
+        },
+        phone: {
+          required: "Пожалуйста, введите номер телефона",
+          minlength: "Формат должен быть +7 (000) 000-00-00",
+        },
+      },
+    });
+  });
+
+
+  //видео
+  var player;
+  $(".video__play-big").on("click", function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+      height: "380",
+      width: "100%",
+      videoId: "RGXpjux1HAA",
+      events: {
+        "onReady": videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event) {
+    event.target.playVideo();
+  }
+
+  // ***********
+  //* scroll to anchor link
+  // ***********
+  $('nav a[href]').click(scrollToAnchor)
+
+  function scrollToAnchor(e) {
+    const anchor = e.target;
+
+    const hash = $(anchor).attr('href');
+    const anchorSection = $(hash);
+
+    if (!anchorSection) return console.error(`${anchor} - not found`);
+
+    $("html, body").animate({
+      scrollTop: anchorSection.offset().top
+    }, 1000)
+
+  }
+  /* 
+   * -------------------
+  */
+
 });
